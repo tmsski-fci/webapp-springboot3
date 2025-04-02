c46024cf6581c140667ac19d53b37b349f01938a
tmsski <20782956+tmsski@users.noreply.github.com>
2025-04-02 12:15:24 +0000

    entidades associadas; início spa

---
+++ references.md
@@ -0,0 +1,3 @@
+https://github.com/tmsasaki/web-restapi-db
+
+https://medium.com/better-programming/js-vanilla-script-spa-1b29b43ea475
---
+++ src/main/java/br/mackenzie/webapp/faculdade/FaculdadeRepo.java
@@ -0,0 +1,7 @@
+package br.mackenzie.webapp.faculdade;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface FaculdadeRepo extends CrudRepository<Faculdade, Long> {
+
+}
